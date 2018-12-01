7b20200596eef52aec8285c47b1ecd00222cf32c
ianertson <sebbekarlsson97@gmail.com>
2018-12-01 17:30:15 +0100

    init

---
+++ 1/main.c
@@ -0,0 +1,15 @@
+#include "../io.h"
+#include <string.h>
+
+
+int main(int argc, char* argv[]) {
+    char* input = read_file("input.txt");
+
+    char* token;
+    int calc = 0;
+    while ((token = strsep(&input, "\n"))) {
+        calc += atoi(token);
+    }
+
+    printf("%d", calc);
+}
